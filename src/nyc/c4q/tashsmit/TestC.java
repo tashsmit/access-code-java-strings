d7e765c26f78adb42b4425a25d7c3a82170bca26
tashsmit <tashsmit91@yahoo.com>
2015-03-01 17:41:57 -0500

    intial commit

---
+++ src/nyc/c4q/tashsmit/TestC.java
@@ -0,0 +1,60 @@
+package nyc.c4q.tashsmit;
+
+/**
+ * Created by c4q-tashasmith on 3/1/15.
+ */
+public class TestC {
+
+    public static void main(String[] args)
+    {
+        /*A comment, this is so you can read your program later.
+        Anything after the // is ignored by Java.*/
+
+        System.out.println( "My letter" ); // and the comment after is ignored.
+
+        /*You can also use a comment to "disable" or comment out a piece of code:
+        System.out.println("This won't run.");*/
+
+        System.out.println( "|----------------------------|" );
+        System.out.println( "|                       :::::|" );
+        System.out.println( "|                        ::::|" );
+        System.out.println( "|                          ::|" );
+        System.out.println( "|    Tasha Smith             |" );
+        System.out.println( "|    620 Thwaites Place      |" );
+        System.out.println( "|    Bronx, NY 10467         |" );
+        System.out.println( "|----------------------------|" );
+
+        System.out.println( "This is a string with \"double quotes\" in it");
+        System.out.println( "Twinkle twinkle little\n" +
+                            "little star" );
+        System.out.println( "How I wonder what \n\n\nYouuu Areeee" );
+
+        System.out.println( "TTTTT  SSS " );
+        System.out.println( "  T   S    " );
+        System.out.println( "  T   S    " );
+        System.out.println( "  T    SSS " );
+        System.out.println( "  T       S" );
+        System.out.println( "  T   S   S" );
+        System.out.println( "  T    SSS " );
+
+        System.out.println("System.out.println(\"Hello, world!\");");
+
+        System.out.println(42);
+        System.out.println("42");
+        System.out.println(42.0);
+        System.out.println(42.0000);
+        System.out.println("42.0000");
+        System.out.println(41.99999999999999999);
+        System.out.println("41.9999999999");
+
+        System.out.println(10 + 30);
+        System.out.println("70" + "1");
+        System.out.println("10" + 30);
+        System.out.println(30 + "10");
+
+        System.out.println("temperature " + (9.0*(-4)/5 + 32) + "\u0277");
+
+
+    }
+}
+
